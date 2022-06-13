9d64c6ce49a2f111ba34d26b2c253e0f98c52b89
Keita Nonaka <iKonnyaku40@gmail.com>
2022-06-13 06:14:46 -0700

    initial commit

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+const Document = () => {
+  return (
+    <Html lang="jp">
+      <Head>
+        <style>{`body {
+            all: unset;
+            font-family: Arial, Helvetica, sans-serif;
+            overflow-y: scroll;
+          }
+          input {
+            outline: 0;
+          }`}</style>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
