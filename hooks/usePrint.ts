e3ffe17a83fde18cd611092417ad10b979ab76e4
Keita Nonaka <iKonnyaku40@gmail.com>
2022-06-25 05:31:25 -0700

    change: comment out pageStyle

--- hooks/usePrint.ts
+++ hooks/usePrint.ts
@@ -6,7 +6,7 @@ export const usePrint = (): [RefObject<HTMLDivElement>, () => void, boolean] => 
   const [isPrintMode, setIsPrintMode] = useState<boolean>(false);
   const onPrint = useReactToPrint({
     content: () => componentRef.current,
-    pageStyle: '@page { size: 10cm 14.8cm; }',
+    // pageStyle: '@page { size: 10cm 14.8cm; }',
     onBeforeGetContent: () => {
       setIsPrintMode(true);
       // wait till the page is changed https://github.com/gregnb/react-to-print/issues/169
